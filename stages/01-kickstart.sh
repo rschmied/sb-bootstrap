294fa297502677c00519cec0068d018ddabc7fd1
Ralph Schmieder <rschmied@cisco.com>
2014-11-21 15:36:33 +0100

    next

--- stages/01-kickstart.sh
+++ stages/01-kickstart.sh
@@ -2,10 +2,11 @@
 
 PATH=/usr/sbin:/usr/bin:/sbin:/bin
 
-. $(dirname)/../etc/config
+. $(dirname $0)/../etc/config
 
 # create the VIRL user 'virl'
 # we don't want a password (use the root key instead)
+useradd -m virl
 usermod -a -G sudo virl
 cp -R /root/.ssh/ /home/virl/
 chown -R virl.virl /home/virl/.ssh/

1e36d2b6437bd2dee9cb988fdec656b0b40e1daa
Ralph Schmieder <rschmied@cisco.com>
2014-11-21 17:45:58 +0100

    some more

--- stages/01-kickstart.sh
+++ stages/01-kickstart.sh
@@ -2,7 +2,8 @@
 
 PATH=/usr/sbin:/usr/bin:/sbin:/bin
 
-. $(dirname $0)/../etc/config
+ORIGIN=$(dirname $0)
+. ${ORIGIN}/../etc/config
 
 # add our hostname to localhost
 sed -i 's/^127.0.0.1 localhost$/& '$(hostname)'/' /etc/hosts
@@ -11,17 +12,19 @@ sed -i 's/^127.0.0.1 localhost$/& '$(hostname)'/' /etc/hosts
 # we don't want a password (use the root key instead)
 useradd -m virl
 usermod -a -G sudo virl
-cp -R /root/.ssh/ /home/virl/
+cp -R /home/ubuntu/.ssh/ /home/virl/
 chown -R virl.virl /home/virl/.ssh/
 
 # clone the VIRL boot strap
-su -c "git clone https://github.com/VIRL-Open/virl-bootstrap.git" - virl 2>&1 | \
-      tee -a $LOGFILE
+su -c "git clone https://github.com/VIRL-Open/virl-bootstrap.git" - virl
 
 # now as root
 # copy and prep private
 # bootstrap the salt minion
 cd /home/virl/virl-bootstrap
+
+# do the salt minion pub/private key stuff
+#
 mkdir -p /etc/salt/pki/minion
 cp ./master_sign.pub /etc/salt/pki/minion
 rm -f ./preseed_keys/minion.pem
@@ -31,6 +34,45 @@ cp -f ./preseed_keys/minion.pem /etc/salt/pki/minion/minion.pem
 cp -f ./preseed_keys/minion.pub /etc/salt/pki/minion/minion.pub
 chmod 400 /etc/salt/pki/minion.pem
 sh /home/virl/virl-bootstrap/bootstrap-salt.sh git 2014.7
+
+# make sure we are connected to the master
+# before we continue
+if [[ $(sudo salt-call test.ping) =~ True ]]; then 
+
+  # make a backup of the interface configuration!
+  cp /etc/network/interfaces /root/interfaces
+
+  # do ZERO
+  sudo salt-call state.sls zero
+
+  # swap the key ID and domain in virl.ini
+  cp ${ORIGIN}/../etc/virl.ini /etc/
+  SALT_DOMAIN=$(basename /tmp/*.pem | cut -d. -f2,3)
+  SALT_ID=$(basename /tmp/*.pem | cut -d. -f1)
+  crudini --set /etc/virl.ini DEFAULT salt_id $SALT_ID
+  crudini --set /etc/virl.ini DEFAULT salt_domain $SALT_DOMAIN
+
+  # do the first stage installation
+  vinstall all
+
+  # need to take care of the interface changes...
+  mv /etc/network/interfaces /etc/network/interfaces-virl
+  cp /root/interfaces /etc/network/interfaces
+  cat /etc/network/interfaces-virl >>/etc/network/interfaces
+
+  # after this, it's time for a reboot
+  # e.g. first stage is done!
+
+else
+
+  cat <<EOF
+	####################
+        salt master problem
+        ####################
+EOF
+
+fi
+
 
 exit
 

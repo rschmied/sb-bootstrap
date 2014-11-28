28b569dfdc7aab091ee7cf65560d97a2c0a5e3dc
Ralph Schmieder <rschmied@cisco.com>
2014-11-28 21:16:02 +0100

    remote 'auto' from dummy0, some more comments

--- stages/04-images.sh
+++ stages/04-images.sh
@@ -27,12 +27,20 @@ sudo salt-call state.sls virl.vmm.vmmall
 # also requires no guest account in virl.ini for the workaround to work
 # guest_account: False
 # this is a workaround (FIXME)
+# PS1=xxx is needed, otherwise .bashrc will exit right away and no OS_* variables
+# are defined.
 su -lc 'PS1=xxx; . ~/.bashrc; virl_uwm_client --username uwmadmin --password password project-create --name guest --user-password guest' virl
 
-# do some misc modification before restarting OpenStack services
-echo "doing misc settings"
+# add external nameservers to guest network
 su -lc 'PS1=xxx; . ~/.bashrc; neutron subnet-update guest --dns_nameservers list=true 8.8.8.8 8.8.4.4' virl
+
+# VMM connects to interface w/ default route (external interface)
+# in this case we want it to show the internal interface
 crudini --set /etc/virl/virl.cfg env virl_local_ip 172.16.1.1
+
+# don't start the dummy0 interface, unused
+# in our case
+sed -i '/auto dummy0/d' /etc/network/interfaces
 
 # restart openstack services (to avoid a restart)
 # echo "restarting OpenStack services"

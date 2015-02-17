81db6d9d6798927337b9f3e40dd1380626ceaca8
Ralph Schmieder <rschmied@cisco.com>
2015-02-17 17:22:08 +0100

    one off package install prior to 'vinstall all'
    plus the libffi-dev package which should be automatically included via
    salt going forward.

--- stages/03-vinstall.sh
+++ stages/03-vinstall.sh
@@ -14,14 +14,14 @@ cd /home/virl/virl-bootstrap
 # check that Salt is available and key is OK
 wait_for_salt
 
-# do all the other VIRL install stages
-echo "install all remaining VIRL packages"
-/usr/local/bin/vinstall all
-
 # packages which are present (and required) in standard
 # Ubuntu server install but are missing on Rackspace.
 echo "install one-off packages"
-apt-get install ethtool
+apt-get install -y ethtool libffi-dev
+
+# do all the other VIRL install stages
+echo "install all remaining VIRL packages"
+/usr/local/bin/vinstall all
 
 exit $STATE_OK
 

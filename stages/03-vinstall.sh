2ceb93d96963daa64e23d313989b5f8726a7dbb7
Ralph Schmieder <rschmied@cisco.com>
2015-04-23 18:33:31 +0200

    one off packages and fixes
    - remove unneeded packages
    - add gawk to replace mawk
    - add vinstall bridge
    - change nova.conf to contain 172.16.1.1 for the serial proxy

--- stages/03-vinstall.sh
+++ stages/03-vinstall.sh
@@ -16,12 +16,25 @@ wait_for_salt
 
 # packages which are present (and required) in standard
 # Ubuntu server install but are missing on Rackspace.
+#
+# ethtool required by LXC container --> "Fix Container"
+# gawk required to make apparmor happy for the telnet_front thing
 echo "install one-off packages"
-apt-get install -y ethtool libffi-dev openstack-dashboard nova-api python-neutronclient
+apt-get install -y ethtool gawk
+
+#apt-get install -y ethtool libffi-dev openstack-dashboard nova-api python-neutronclient
 
 # do all the other VIRL install stages
 echo "install all remaining VIRL packages"
 /usr/local/bin/vinstall all
+
+# fix Linux bridging (only temporary needed, should go into all eventually)
+/usr/local/bin/vinstall bridge
+
+# fix the bind port in nova.conf
+# otherwise it will listen on the outside, public facing IP which is firewalled)
+echo "configuring nova.conf"
+crudini --set /etc/nova/nova.conf DEFAULT serial_port_proxyclient_address 172.16.1.1
 
 exit $STATE_OK
 
